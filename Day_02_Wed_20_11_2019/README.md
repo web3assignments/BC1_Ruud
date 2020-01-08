7590cfa0f6e1ecaa0e14ad9a49fc7eeaf570d113
2-cluster <admin@2cluster.com>
2020-01-08 11:51:12 +0100

    inbox & lottery contract with rinkeby deployment using infura

--- Day_02_Wed_20_11_2019/README.md
+++ Day_02_Wed_20_11_2019/README.md
@@ -1,2 +1,8 @@
 ### Day_02_Wed_20_11_2019 
-Place the assignments in this subdirectory   
+I have created a inbox contract & lottery contract with a test setup using 'mocha'
+
+I have deployed the contract on the Rinkeby test network using Infura to get it on the network
+
+Then using remix i can make use of the contract with my metamask account
+
+In the proof folder i stored the screenshots
---
+++ Day_02_Wed_20_11_2019/lottery/deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'question lava deny maze hurt body obtain tail capable receive element rug',
+  'https://rinkeby.infura.io/v3/56d2c50e219f40d78a0b05fedee1bfa4'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
